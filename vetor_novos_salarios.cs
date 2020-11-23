8e510fd7c5cfed1e8fdbe179ac70dc80b1f82a7d
Érika Morais Xavier <52464555+rikamorais@users.noreply.github.com>
2020-11-23 18:02:01 -0300

    Add files via upload

---
+++ vetor_novos_salarios.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vetor_novos_salarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Crie um algoritmo que leia um vetor de salários de 8 posições, calcule e imprima
+             o vetor com os novos salários, reajustados em 12% (aumento de 12%)*/
+
+            double i;
+            double[] v = new double[8];
+
+            Console.WriteLine("|----------------------------------------|");
+            Console.WriteLine("|--------- Reajuste de salários ---------|");
+            Console.WriteLine("|----------------------------------------|\n");
+
+            Console.WriteLine("Digite os elementos do vetor: ");
+
+            for (i = 0; i < 8; i++)
+            {
+                Console.Write("v[" + i + "]:");
+                v[i] = Convert.ToDouble(Console.ReadLine());
+            }// fim do for
+
+            Console.Clear();
+
+            for (i = 0; i < 8; i++)
+            {
+                Console.WriteLine("Salário atual: R$ " + v[i] + "| Novo salário: R$ " + v[i] * 1.12);
+            }
+
+            Console.ReadKey();
+        }// fim da classe principal
+    }// fim da classe
+}// fim do algoritmo
---
+++ vetor_pares_impares.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vetor_pares_impares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {/*Faça um programa que carregue um vetor de seis elementos inteiros, calcule e mostre
+          - a quantidade de números pares
+          - quais os números pares
+          - a quantidade de números ímpares
+          - quais os números ímpares*/
+
+        int i, cont_par=0, cont_impar=0;
+        int[] v = new int[6];
+
+        Console.WriteLine("|-----------------------------------------|");
+        Console.WriteLine("|--- Vetor - elementos pares e ímpares ---|");
+        Console.WriteLine("|-----------------------------------------|\n");
+        Console.WriteLine("Digite os elementos do vetor:");
+        for (i=0; i<6; i++)
+            {
+                Console.WriteLine("Digite v[" + i + "]:");
+                v[i] = Convert.ToInt32(Console.ReadLine());
+
+                if (v[i] % 2 == 0)
+                {
+                    cont_par++;
+                }
+                else
+                {
+                    cont_impar++;
+                }
+            }// fim do for
+         
+        Console.WriteLine("Foram digitados " + cont_par + " números pares:");
+
+            // ------------------------- PARES ------------------------        
+            if (cont_par > 0)
+            {
+                for (i=0; i<6; i++)
+                {
+                    if (v[i] % 2 == 0)
+                    {
+                        Console.Write(v[i] + "\t");
+                    }
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("Não foram digitados números pares.");
+            }
+            // ----------------------- IMPARES --------------------
+
+            Console.Write("\n\nForam digitados " + cont_impar + " números ímpares:");
+            if (cont_impar > 0)
+            {
+                for (i=0; i<6; i++)
+                {
+                    if (v[i] % 2 != 0) {
+                        Console.WriteLine(v[i] + "\t");
+                    }
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Não foram digitados números ímpares.");
+            }
+     
+
+            Console.ReadKey();
+
+        }// fima da classe principal
+    }
+}
